6358d8dac795888b5a28518f8fd06041aa3ca490
(oré) <79204651+aurelienlouvel@users.noreply.github.com>
2023-04-28 19:03:37 +0200

    ✨(header): add content

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <a class="logo" href="https://aurelienlouvel.fr">
+            <img src="src/assets/images/logos/aurelienlouvel.svg">
+        </a>
+        <p class="label">©2023</p>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+
+</style>
