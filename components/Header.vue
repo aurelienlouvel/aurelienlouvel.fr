6541dda372025f460b7d84aad3611b4849d40da7
(oré) <79204651+aurelienlouvel@users.noreply.github.com>
2023-04-28 19:07:48 +0200

    🍱(header): add logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,9 @@
 <template>
     <header>
         <a class="logo" href="https://aurelienlouvel.fr">
-            <img src="src/assets/images/logos/aurelienlouvel.svg">
+            <svg width="14" height="18" viewBox="0 0 14 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.52423 1.16855C3.89575 0.452385 4.65519 0 5.48592 0H11.8068C13.0181 0 14 0.946435 14 2.11392V15.8823C14 18.0062 11.1148 18.8037 9.9469 17.0027L6.41962 11.5632C6.01883 10.9451 5.31597 10.5696 4.55976 10.5696H2.19607C0.565654 10.5696 -0.494767 8.91586 0.234376 7.51031L3.52423 1.16855Z" fill="#040810"/>
+            </svg>
         </a>
         <p class="label">©2023</p>
     </header>
