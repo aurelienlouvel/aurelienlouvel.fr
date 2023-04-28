319352a93dc955ef0f4ca5d2016f0a50e51122b6
(oré) <79204651+aurelienlouvel@users.noreply.github.com>
2023-04-28 19:03:54 +0200

    ✨(footer): add content

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+        <p class="contact__title">Contact</p>
+        <a href="mailto:louvel.aurelien.pro@gmail.com" class="contact contact__link">(louvel.aurelien.pro@gmail.com)</a>
+
+        <p class="socials__title">Socials</p>
+        <div class="socials__list">
+            <a href="https://bento.me/aurelienlouvel" class="socials__link">(Bento)</a>
+            <a href="https://twitter.com/aurelienlouvel" class="socials__link">(Twitter)</a>
+            <a href="https://www.linkedin.com/in/aurelienlouvel/" class="socials__link">(LinkedIn)</a>
+            <a href="https://github.com/aurelienlouvel" class="socials__link">(GitHub)</a>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+
+</style>
