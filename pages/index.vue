f620c91f9d01f77a3b3ab54b4e225fc71409c04a
(oré) <79204651+aurelienlouvel@users.noreply.github.com>
2023-04-29 15:14:19 +0200

    🎨(index): remove scoped classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,31 @@
 <template>
     <div>
         <Header/>
-        <main>
-            <p class="alias">(oré)</p>
+        <main class="home grid">
+            <h2 class="home__alias">(oré)</h2>
 
-            <h4 class="one">01</h4>
-            <h3 class="about">I’m a front-end web developer <i>& designer,</i><br>studying at Gobelins Paris, France.</h3>
+            <h5 class="home__number home--about">01</h5>
+            <h3 class="home__content home--about">I’m a front-end web developer <i>& designer,</i><br>studying at
+                Gobelins Paris, France.
+            </h3>
 
-            <h4 class="two">02</h4>
-            <h3 class="apprenticeship">Currently looking for a new<br>apprenticeship starting in<br>September 2023.</h3>
+            <h5 class="home__number home--apprenticeship">02</h5>
+            <h3 class="home__content home--apprenticeship">Currently looking for a new<br>apprenticeship starting in<br>September
+                2023.</h3>
 
-            <h4 class="three">03</h4>
-            <div class="document__list">
-                <a href="#" class="document__link">
-                    <h3 class="document__name resume">Resume</h3>
-                    <span class="document__arrow">↗</span>
+            <h5 class="home__number home--document">03</h5>
+            <div class="home__content home--document col">
+                <a class="link document--resume" href="#">
+                    <h3 class="link__name">Resume</h3>
+                    <span class="link__arrow">↗</span>
                 </a>
-                <a href="#" class="document__link">
-                    <h3 class="document__name portfolio">Portfolio</h3>
-                    <span class="document__arrow">↗</span>
+                <a class="link document--portfolio" href="#">
+                    <h3 class="link__name">Portfolio</h3>
+                    <span class="link__arrow">↗</span>
                 </a>
             </div>
 
-            <div class="title">
+            <div class="home__title">
                 <div class="title__row">
                     <h1 class="title__text">Interactive</h1>
                     <div class="title__symbol">
@@ -41,8 +44,66 @@
         <Background/>
     </div>
 </template>
-<script setup lang="ts">
+
+<script lang="ts" setup>
 import Header from "~/components/Header.vue"
 import Footer from "~/components/Footer.vue"
 import Background from "~/components/Background.vue"
 </script>
+
+<style lang="scss">
+.home {
+  $self: &;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100dvh;
+  width: 100%;
+  grid-template-rows: 0.175fr 0.225fr auto;
+
+  pointer-events: none;
+
+  &__alias {
+    grid-area: 2/1/3/3;
+  }
+
+  &__number {
+    padding: 1px 0;
+    justify-self: end;
+
+    &#{$self}--about {
+      grid-area: 2/2/3/3;
+    }
+
+    &#{$self}--apprenticeship {
+      grid-area: 2/6/3/7;
+    }
+
+    &#{$self}--document {
+      grid-area: 3/6/4/7;
+    }
+  }
+
+  &__content {
+
+    &#{$self}--about {
+      grid-area: 2/3/3/6;
+    }
+
+    &#{$self}--apprenticeship {
+      grid-area: 2/7/3/10;
+    }
+
+    &#{$self}--document {
+      grid-area: 3/7/4/10;
+    }
+  }
+
+  &__title {
+    grid-area: 3/1/4/8;
+    align-self: end;
+  }
+
+}
+
+</style>
