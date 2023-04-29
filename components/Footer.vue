4c4871fea266752b882d0753f3e63d1c8c6dda5e
(oré) <79204651+aurelienlouvel@users.noreply.github.com>
2023-04-29 14:27:31 +0200

    🎨(footer): optimize classes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,17 @@
 <template>
     <footer class="footer grid">
-        <p class="footer__title contact">Contact</p>
-        <div class="footer__list contact">
-            <a class="footer__link" href="mailto:louvel.aurelien.pro@gmail.com">(louvel.aurelien.pro@gmail.com)</a>
+        <h5 class="footer__title footer--contact">Contact</h5>
+        <div class="footer__links footer--contact col">
+            <a class="link" href="mailto:louvel.aurelien.pro@gmail.com"
+               target="_blank">(louvel.aurelien.pro@gmail.com)</a>
         </div>
 
-        <p class="footer__title socials">Socials</p>
-        <div class="footer__list socials">
-            <a class="footer__link" href="https://bento.me/aurelienlouvel">(Bento)</a>
-            <a class="footer__link" href="https://twitter.com/aurelienlouvel">(Twitter)</a>
-            <a class="footer__link" href="https://www.linkedin.com/in/aurelienlouvel/">(LinkedIn)</a>
-            <a class="footer__link" href="https://github.com/aurelienlouvel">(GitHub)</a>
+        <h5 class="footer__title footer--socials">Socials</h5>
+        <div class="footer__links footer--socials col">
+            <a class="link" href="https://bento.me/aurelienlouvel" target="_blank">(Bento)</a>
+            <a class="link" href="https://twitter.com/aurelienlouvel" target="_blank">(Twitter)</a>
+            <a class="link" href="https://www.linkedin.com/in/aurelienlouvel/" target="_blank">(LinkedIn)</a>
+            <a class="link" href="https://github.com/aurelienlouvel" target="_blank">(GitHub)</a>
         </div>
     </footer>
 </template>
@@ -21,40 +22,39 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 .footer {
+  $self: &;
   position: absolute;
   bottom: 0;
   align-items: end;
+  pointer-events: none;
+
+  & > * {
+    pointer-events: all;
+  }
 
   &__title {
-    font-size: var(--font-size-tiny);
-    font-weight: var(--font-weight-medium);
     justify-self: end;
-    color: var(--color-neutral-20);
 
-    &.contact {
+    &#{$self}--contact {
       grid-column: 7/8;
     }
 
-    &.socials {
+    &#{$self}--socials {
       grid-column: 10/11;
     }
   }
 
-  &__list {
-    display: flex;
-    flex-direction: column;
-    gap: 4px;
-
-    &.contact {
+  &__links {
+    &#{$self}--contact {
       grid-column: 8/10;
     }
-  }
 
-  &__link {
-    font-size: var(--font-size-tiny);
+    &#{$self}--socials {
+      grid-column: 11/13;
+    }
   }
 }
 
